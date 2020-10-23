6674daf4a23eda0056214d173c8e937a6c2897c9
Adarsha HD <me@adarshahd.com>
2020-10-23 16:58:36 +0530

    Updated the generated routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,48 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('settings', App\Http\Controllers\Api\AppSettingController::class);
+
+Route::apiResource('id-types', App\Http\Controllers\Api\IdTypeController::class);
+
+Route::apiResource('users', App\Http\Controllers\Api\UserController::class);
+
+Route::apiResource('customers', App\Http\Controllers\Api\CustomerController::class);
+
+Route::apiResource('vendors', App\Http\Controllers\Api\VendorController::class);
+
+Route::apiResource('manufacturers', App\Http\Controllers\Api\ManufacturerController::class);
+
+Route::apiResource('taxes', App\Http\Controllers\Api\TaxController::class);
+
+Route::apiResource('product-types', App\Http\Controllers\Api\ProductTypeController::class);
+
+Route::apiResource('products', App\Http\Controllers\Api\ProductController::class);
+
+Route::apiResource('products.stock', App\Http\Controllers\Api\ProductStockController::class);
+
+Route::apiResource('purchases', App\Http\Controllers\Api\PurchaseController::class);
+
+Route::apiResource('purchases.items', App\Http\Controllers\Api\PurchaseItemController::class);
+
+Route::apiResource('sales', App\Http\Controllers\Api\SaleController::class);
+
+Route::apiResource('sales.items', App\Http\Controllers\Api\SaleItemController::class);
+
+Route::apiResource('debits', App\Http\Controllers\Api\DebitController::class);
+
+Route::apiResource('debits.items', App\Http\Controllers\Api\DebitItemController::class);
+
+Route::apiResource('credits', App\Http\Controllers\Api\CreditController::class);
+
+Route::apiResource('credits.items', App\Http\Controllers\Api\CreditItemController::class);
+
+Route::apiResource('receipts', App\Http\Controllers\Api\ReceiptController::class);
+
+Route::apiResource('vouchers', App\Http\Controllers\Api\VoucherController::class);
+
+Route::apiResource('incomes', App\Http\Controllers\Api\IncomeController::class);
+
+Route::apiResource('expenses', App\Http\Controllers\Api\ExpenseController::class);
