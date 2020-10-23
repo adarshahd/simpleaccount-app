f13caa9ae168b6f5a6af0efa69f9dcc69836ef7c
Adarsha HD <me@adarshahd.com>
2020-10-23 19:25:35 +0530

    Removed controllers which were not needed; Cleaned up api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,52 +12,25 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function() {
+    Route::apiResources([
+        'settings' => App\Http\Controllers\Api\AppSettingController::class,
+        'id-types' => App\Http\Controllers\Api\IdTypeController::class,
+        'users' => App\Http\Controllers\Api\UserController::class,
+        'customers' => App\Http\Controllers\Api\CustomerController::class,
+        'vendors' => App\Http\Controllers\Api\VendorController::class,
+        'manufacturers' => App\Http\Controllers\Api\ManufacturerController::class,
+        'taxes' => App\Http\Controllers\Api\TaxController::class,
+        'product-types' => App\Http\Controllers\Api\ProductTypeController::class,
+        'products' => App\Http\Controllers\Api\ProductController::class,
+        'products.stock' => App\Http\Controllers\Api\ProductStockController::class,
+        'purchases' => App\Http\Controllers\Api\PurchaseController::class,
+        'sales' => App\Http\Controllers\Api\SaleController::class,
+        'debits' => App\Http\Controllers\Api\DebitController::class,
+        'credits' => App\Http\Controllers\Api\CreditController::class,
+        'receipts' => App\Http\Controllers\Api\ReceiptController::class,
+        'vouchers' => App\Http\Controllers\Api\VoucherController::class,
+        'incomes' => App\Http\Controllers\Api\IncomeController::class,
+        'expenses' => App\Http\Controllers\Api\ExpenseController::class,
+    ]);
 });
-
-
-Route::apiResource('settings', App\Http\Controllers\Api\AppSettingController::class);
-
-Route::apiResource('id-types', App\Http\Controllers\Api\IdTypeController::class);
-
-Route::apiResource('users', App\Http\Controllers\Api\UserController::class);
-
-Route::apiResource('customers', App\Http\Controllers\Api\CustomerController::class);
-
-Route::apiResource('vendors', App\Http\Controllers\Api\VendorController::class);
-
-Route::apiResource('manufacturers', App\Http\Controllers\Api\ManufacturerController::class);
-
-Route::apiResource('taxes', App\Http\Controllers\Api\TaxController::class);
-
-Route::apiResource('product-types', App\Http\Controllers\Api\ProductTypeController::class);
-
-Route::apiResource('products', App\Http\Controllers\Api\ProductController::class);
-
-Route::apiResource('products.stock', App\Http\Controllers\Api\ProductStockController::class);
-
-Route::apiResource('purchases', App\Http\Controllers\Api\PurchaseController::class);
-
-Route::apiResource('purchases.items', App\Http\Controllers\Api\PurchaseItemController::class);
-
-Route::apiResource('sales', App\Http\Controllers\Api\SaleController::class);
-
-Route::apiResource('sales.items', App\Http\Controllers\Api\SaleItemController::class);
-
-Route::apiResource('debits', App\Http\Controllers\Api\DebitController::class);
-
-Route::apiResource('debits.items', App\Http\Controllers\Api\DebitItemController::class);
-
-Route::apiResource('credits', App\Http\Controllers\Api\CreditController::class);
-
-Route::apiResource('credits.items', App\Http\Controllers\Api\CreditItemController::class);
-
-Route::apiResource('receipts', App\Http\Controllers\Api\ReceiptController::class);
-
-Route::apiResource('vouchers', App\Http\Controllers\Api\VoucherController::class);
-
-Route::apiResource('incomes', App\Http\Controllers\Api\IncomeController::class);
-
-Route::apiResource('expenses', App\Http\Controllers\Api\ExpenseController::class);
