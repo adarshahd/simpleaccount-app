bf0c5fce3d3b044859607fe3254dd10c8a841aa7
Adarsha HD <me@adarshahd.com>
2020-11-17 16:18:29 +0530

    FIX: Included sanctum middleware into needed routes

--- routes/api.php
+++ routes/api.php
@@ -21,11 +21,11 @@ Route::group(['prefix' => 'v1'], function() {
     Route::post('/users/admin', [\App\Http\Controllers\OnboardController::class, 'createAdminUser']);
     Route::post('/product-owner', [\App\Http\Controllers\OnboardController::class, 'updateProductOwnerData']);
 
-    /*
-     * Route to get current logged in user
-     * */
     Route::group(['middleware' => 'auth:sanctum'], function() {
 
+        /*
+         * Route to get current logged in user
+         * */
         Route::get('/user', function(Request $request) {
             return response()->json([
                 'data' => [
@@ -34,48 +34,48 @@ Route::group(['prefix' => 'v1'], function() {
             ]);
         });
 
+        /*
+         * Search Routes
+         * */
+        Route::get('/customers/search', [\App\Http\Controllers\Api\CustomerController::class, 'find']);
+        Route::get('/vendors/search', [\App\Http\Controllers\Api\VendorController::class, 'find']);
+        Route::get('/products/search', [\App\Http\Controllers\Api\ProductController::class, 'find']);
+        Route::get('products/{product}/stock/search', [\App\Http\Controllers\Api\ProductController::class, 'stock']);
+        Route::get('/manufacturers/search', [\App\Http\Controllers\Api\ManufacturerController::class, 'find']);
+
+        /*
+         * Extra routes
+         * */
+
+        Route::get('/customers/{customer}/details', [\App\Http\Controllers\Api\CustomerController::class, 'details']);
+        Route::get('/vendors/{vendor}/details', [\App\Http\Controllers\Api\VendorController::class, 'details']);
+        Route::get('/sales/{sale}/invoice', [\App\Http\Controllers\Api\SaleController::class, 'invoice']);
+        Route::get('/credits/{credit}/invoice', [\App\Http\Controllers\Api\CreditController::class, 'invoice']);
+
+        /*
+         * API Resource Routes
+         * */
+        Route::apiResources([
+            'settings' => App\Http\Controllers\Api\AppSettingController::class,
+            'id-types' => App\Http\Controllers\Api\IdTypeController::class,
+            'users' => App\Http\Controllers\Api\UserController::class,
+            'customers' => App\Http\Controllers\Api\CustomerController::class,
+            'vendors' => App\Http\Controllers\Api\VendorController::class,
+            'manufacturers' => App\Http\Controllers\Api\ManufacturerController::class,
+            'taxes' => App\Http\Controllers\Api\TaxController::class,
+            'product-types' => App\Http\Controllers\Api\ProductTypeController::class,
+            'products' => App\Http\Controllers\Api\ProductController::class,
+            'products.stock' => App\Http\Controllers\Api\ProductStockController::class,
+            'purchases' => App\Http\Controllers\Api\PurchaseController::class,
+            'sales' => App\Http\Controllers\Api\SaleController::class,
+            'debits' => App\Http\Controllers\Api\DebitController::class,
+            'credits' => App\Http\Controllers\Api\CreditController::class,
+            'receipts' => App\Http\Controllers\Api\ReceiptController::class,
+            'vouchers' => App\Http\Controllers\Api\VoucherController::class,
+            'incomes' => App\Http\Controllers\Api\IncomeController::class,
+            'expenses' => App\Http\Controllers\Api\ExpenseController::class,
+            'orders' => App\Http\Controllers\Api\OrderController::class
+        ]);
+
     });
-
-    /*
-     * Search Routes
-     * */
-    Route::get('/customers/search', [\App\Http\Controllers\Api\CustomerController::class, 'find']);
-    Route::get('/vendors/search', [\App\Http\Controllers\Api\VendorController::class, 'find']);
-    Route::get('/products/search', [\App\Http\Controllers\Api\ProductController::class, 'find']);
-    Route::get('products/{product}/stock/search', [\App\Http\Controllers\Api\ProductController::class, 'stock']);
-    Route::get('/manufacturers/search', [\App\Http\Controllers\Api\ManufacturerController::class, 'find']);
-
-    /*
-     * Extra routes
-     * */
-
-    Route::get('/customers/{customer}/details', [\App\Http\Controllers\Api\CustomerController::class, 'details']);
-    Route::get('/vendors/{vendor}/details', [\App\Http\Controllers\Api\VendorController::class, 'details']);
-    Route::get('/sales/{sale}/invoice', [\App\Http\Controllers\Api\SaleController::class, 'invoice']);
-    Route::get('/credits/{credit}/invoice', [\App\Http\Controllers\Api\CreditController::class, 'invoice']);
-
-    /*
-     * API Resource Routes
-     * */
-    Route::apiResources([
-        'settings' => App\Http\Controllers\Api\AppSettingController::class,
-        'id-types' => App\Http\Controllers\Api\IdTypeController::class,
-        'users' => App\Http\Controllers\Api\UserController::class,
-        'customers' => App\Http\Controllers\Api\CustomerController::class,
-        'vendors' => App\Http\Controllers\Api\VendorController::class,
-        'manufacturers' => App\Http\Controllers\Api\ManufacturerController::class,
-        'taxes' => App\Http\Controllers\Api\TaxController::class,
-        'product-types' => App\Http\Controllers\Api\ProductTypeController::class,
-        'products' => App\Http\Controllers\Api\ProductController::class,
-        'products.stock' => App\Http\Controllers\Api\ProductStockController::class,
-        'purchases' => App\Http\Controllers\Api\PurchaseController::class,
-        'sales' => App\Http\Controllers\Api\SaleController::class,
-        'debits' => App\Http\Controllers\Api\DebitController::class,
-        'credits' => App\Http\Controllers\Api\CreditController::class,
-        'receipts' => App\Http\Controllers\Api\ReceiptController::class,
-        'vouchers' => App\Http\Controllers\Api\VoucherController::class,
-        'incomes' => App\Http\Controllers\Api\IncomeController::class,
-        'expenses' => App\Http\Controllers\Api\ExpenseController::class,
-        'orders' => App\Http\Controllers\Api\OrderController::class
-    ]);
 });
