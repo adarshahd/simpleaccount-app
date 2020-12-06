c1d27392d775c36fe4ba2029eccc9b7a785c8cd2
Adarsha HD <me@adarshahd.com>
2020-12-06 22:24:59 +0530

    Logo Updated
    Updated logo files and references to brand new logo!

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -1,8 +1,20 @@
 <template>
     <nav class="navbar is-primary has-shadow is-fixed-top" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
-            <div class="navbar-item">
-                <h1 class="title">SimpleAccount</h1>
+            <div class="navbar-item is-expanded">
+                <div class="columns is-vcentered is-gapless is-hidden-mobile" style="margin-bottom: 0 !important">
+                    <div class="column is-2">
+                        <figure class="figure">
+                            <img class="image" src="/images/logo-dark.png" alt="logo">
+                        </figure>
+                    </div>
+                    <div class="column is-10 ml-2">
+                        <h4 class="title is-4">SimpleAccount</h4>
+                    </div>
+                </div>
+                <figure class="figure is-hidden-desktop is-hidden-fullhd is-fullwidth">
+                    <img class="image" src="/images/logo-mobile-dark.png" width="60" height="50" alt="logo">
+                </figure>
             </div>
 
             <a
@@ -20,7 +32,7 @@
 
         <div class="navbar-menu">
             <div class="navbar-start">
-                <div class="navbar-item">
+                <div class="navbar-item is-expanded">
                     <b-field>
                         <b-autocomplete
                             icon-right="mdi mdi-search-web mdi-24px has-text-primary has-text-weight-bold"
