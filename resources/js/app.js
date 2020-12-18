c5fb89d8ec9b9a89668596d8410be77038816e1c
Adarsha HD <me@adarshahd.com>
2020-12-18 16:26:00 +0530

    Buefy Component Import Refactor
    Now importing only required Buefy components which reduces the final
    javascript application size.
    
    Also refactored application loading.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,21 @@
 import Vue from 'vue'
 import axios from 'axios'
-import Buefy from 'buefy'
+import { Table, Toast, Datepicker, Collapse, Autocomplete, Field, Icon } from 'buefy'
 
 import router from './router'
 import store from './store'
 
 axios.defaults.withCredentials = true
 
-Vue.use(Buefy, {
-    defaultLocale: 'en-IN'
-})
+Vue.use(Table, {defaultLocale: 'en-IN'})
+Vue.use(Toast, {defaultLocale: 'en-IN'})
+Vue.use(Datepicker, {defaultLocale: 'en-IN'})
+Vue.use(Collapse, {defaultLocale: 'en-IN'})
+Vue.use(Autocomplete, {defaultLocale: 'en-IN'})
+Vue.use(Field, {defaultLocale: 'en-IN'})
+Vue.use(Icon, {defaultLocale: 'en-IN'})
 
-store.dispatch("getCurrentUser").then(response => {
-    const app = new Vue({
-        router,
-        store
-    }).$mount("#app")
-}).catch(error => {
+store.dispatch("getCurrentUser").finally(() => {
     const app = new Vue({
         router,
         store
